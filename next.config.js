eba021e79b2bb4eafa3596fbb77870af205fc7de
Tejas Bhati <tejasbhati112002@gmail.com>
2024-01-25 17:02:48 +0530

    Update next.config.js for static export

--- next.config.js
+++ next.config.js
@@ -1,7 +1,13 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-    output: 'export',
-
-}
-
-module.exports = nextConfig
+// next.config.js
+module.exports = {
+    // Your existing configuration options go here
+  
+    // Add the following export configuration
+    exportPathMap: async function () {
+      return {
+        '/': { page: '/' },
+        // Add other paths as needed
+      };
+    },
+  };
+  
