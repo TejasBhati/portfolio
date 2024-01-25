24ae352bd82e39dd822b54e9a3df9b2b7d657211
Tejas Bhati <tejasbhati112002@gmail.com>
2024-01-25 21:32:48 +0530

    node version updated

--- next.config.js
+++ next.config.js
@@ -1,7 +1,8 @@
-/** @type {import('next').NextConfig} */
+/**
+ * @type {import('next').NextConfig}
+ */
 const nextConfig = {
-    output: 'export',
-
+    /* config options here */
 }
 
-export default nextConfig
+module.exports = nextConfig
