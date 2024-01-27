4071fbacb3ac96533a33d4ee73f0534e37543f01
Tejas Bhati <tejasbhati112002@gmail.com>
2024-01-28 00:07:18 +0530

    image optimization error resolving

--- next.config.js
+++ next.config.js
@@ -1,7 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     images: {
-        unoptimized: true,
+        loader: "imgix",
+        path: "",
       },
     output: 'export',
 }
