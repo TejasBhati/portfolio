f15466f6e8c8a2d24f855fe70f366160148bf7e7
Tejas Bhati <88779332+TejasBhati@users.noreply.github.com>
2024-02-05 21:16:48 +0530

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,16 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  // images: {
-  //     loader: 'imgix',
-  //     path: '/',
-  //   },
+
   images: { unoptimized: true },
   output: 'export',
 
 
-    // Optional: Change the output directory `out` -> `dist`
-  // distDir: 'dist',
-  // basePath: '/portfolio',
+ // basePath: '/portfolio',
 }
 
 module.exports = nextConfig
