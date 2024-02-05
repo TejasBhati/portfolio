e568c08871b40ee47bfb71f0868bc65015749335
Tejas Bhati <88779332+TejasBhati@users.noreply.github.com>
2024-02-05 21:48:02 +0530

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,6 +1,16 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
 
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'github.com',
+       
+      },
+    ],
+  },
+
   // output: "export",  // <=== enables static exports
   reactStrictMode: true,
 
