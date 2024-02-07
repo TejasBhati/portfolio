edfd2c9121a0fb5e876f298501ee361e6d0105d1
Tejas Bhati <88779332+TejasBhati@users.noreply.github.com>
2024-02-07 23:04:07 +0530

    Update Logo.jsx

--- components/Logo.jsx
+++ components/Logo.jsx
@@ -4,8 +4,12 @@ import Image from 'next/image';
 const Logo = () => {
   return (
     <Link href='/'>
+
+      
     
-        <Image src='/logo_tejas.svg' width={200} height={200} priority alt='' />
+{/*         <Image src='/logo_tejas.svg' width={200} height={200} priority alt='' /> */}
+       <Image src='https://github.com/TejasBhati/portfolio/blob/main/public/logo_tejas.svg' width={200} height={200} priority alt='' />
+     
      
     </Link>
   );
