8ae6f91e81831c1ce5eb39b7d78d242ae35ce2c2
Tejas Bhati <tejasbhati112002@gmail.com>
2024-02-07 23:21:27 +0530

    next js config updated

--- components/Logo.jsx
+++ components/Logo.jsx
@@ -1,6 +1,6 @@
 import Link from 'next/link';
 import Image from 'next/image';
-import TejasLogo from '@/public/logo_tejas.svg'
+import TejasLogo from '../public/logo_tejas.svg'
 
 const Logo = () => {
   return (
