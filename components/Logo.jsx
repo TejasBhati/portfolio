25d48c54d5473e450097994d97b7780cd5101ac8
Tejas Bhati <tejasbhati112002@gmail.com>
2024-02-10 23:41:53 +0530

    logo jsx updated

--- components/Logo.jsx
+++ components/Logo.jsx
@@ -1,14 +1,11 @@
 import Link from 'next/link';
 import Image from 'next/image';
-import TejasLogo from '../public/logo_tejas.svg'
 
 const Logo = () => {
   return (
     <Link href='/'>
-     
     
-        <Image src={TejasLogo} width={200} height={200} priority alt='' />
-     
+        <Image src='/tejaslogo.svg' width={200} height={200} priority alt='TEJASBHATI'  quality={90} />
      
     </Link>
   );
